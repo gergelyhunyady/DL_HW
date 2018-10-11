142d20b02459a4a7062f5fb6bfcbba3a1ed2c8d1
Hunyi <23hunyi@gmail.com>
2018-10-11 22:35:18 +0200

    Plot javítva (y-z) + adatok dictbe mentve

--- Code/data_loader.py
+++ Code/data_loader.py
@@ -16,6 +16,8 @@ pd.set_option('display.width', 1000)
 # --------------------------------------------------- Actual code ------------------------------------------------------
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
+data_dict = {'IMU': {}, 'MoCap': {}}
+
 for FILE_NAME in os.listdir(DIR_PATH[:-5] + '\\DATA\\RAW\\Measure_01'):
     print(FILE_NAME)
     if FILE_NAME[-7:-4] == 'IMU':
@@ -23,16 +25,21 @@ for FILE_NAME in os.listdir(DIR_PATH[:-5] + '\\DATA\\RAW\\Measure_01'):
                                sep='\t',
                                decimal=',',
                                names=['time', 'acc0', 'acc1', 'acc2', 'gyro0', 'gyro1', 'gyro2', 'mag0', 'mag1', 'mag2'])
+        data_dict['IMU'][FILE_NAME[:-8]] = imu_data
         print(imu_data.head())
         f, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1)
         imu_data.plot(x='time', y='mag0', ax=ax1)
         imu_data.plot(x='time', y='mag1', ax=ax2)
         imu_data.plot(x='time', y='mag2', ax=ax3)
+
     elif FILE_NAME[-9:-4] == 'MoCap':
         mocap_data = pd.read_csv(DIR_PATH[:-5] + '\\DATA\\RAW\\Measure_01\\' + FILE_NAME,
                                  sep='\t',
                                  decimal=',',
                                  names=['time', 'x', 'y', 'z', 'tracked', 'zeta'])
-        print(mocap_data.head())
-        mocap_data.plot(x='x', y='y', ax=ax4)
+        data_dict['MoCap'][FILE_NAME[:-10]] = mocap_data
+        print(mocap_data.describe())
+        mocap_data.plot(x='x', y='z', ax=ax4)
         plt.show()
+
+print(data_dict)
